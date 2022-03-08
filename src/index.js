6a685c85f67956b40a0c2bbda9151458bb228255
Eli Shagam <eli.shagam@gmail.com>
2022-03-08 20:00:03 +0200

    First commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
 require('dotenv').config();
 console.log (process.env)
-
 
 ReactDOM.render(
   <React.StrictMode>
